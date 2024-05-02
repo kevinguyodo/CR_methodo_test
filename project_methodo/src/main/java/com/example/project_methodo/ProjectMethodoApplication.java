745ff4244799ec33ed6d84f6709c5928df28af08
Kévin Guyodo <kevin.guyodo@ynov.com>
2024-05-02 15:54:11 +0200

    init project

---
+++ project_methodo/src/main/java/com/example/project_methodo/ProjectMethodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_methodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMethodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMethodoApplication.class, args);
+	}
+
+}
---
+++ project_methodo/src/test/java/com/example/project_methodo/ProjectMethodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_methodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMethodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
